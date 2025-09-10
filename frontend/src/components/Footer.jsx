9ab4730116c1e599b0ad71732c36d99048455db9
ndt-dinhtu <ndt.290303060703>
2025-09-11 05:16:31 +0700

    Tao footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { act } from "react";
 
-const Footer = () => {
+const Footer = ({ completedTaskCount = 0, activeTaskCount = 0 }) => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+      {completedTaskCount + activeTaskCount > 0 && (
+        <div className="text-center">
+          <p className="text-sm text-muted-foreground">
+            {completedTaskCount > 0 && (
+              <>
+                Tuyệt vời, bạn đã hoành thành {completedTaskCount} việc
+                {activeTaskCount > 0 &&
+                  `, còn ${activeTaskCount} việc nữa thôi, cố lên!!`}
+              </>
+            )}
 
-export default Footer
+            {completedTaskCount === 0 && activeTaskCount > 0 && (
+              <>Hãy bắt đầu làm {activeTaskCount} việc đi nào!!</>
+            )}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Footer;
